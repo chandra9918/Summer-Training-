c25071a1d8502f4513a05ff1ad77d167d53d5ab3
Chandra Prakash <prakashchandra7052@gmail.com>
2026-06-18 10:24:21 +0530

    update kiya

---
+++ FD10/datatype.js
@@ -0,0 +1,45 @@
+//Primitive data type  --> String , NUumber , Boolean, Null, Undefined, Bigint, Symbol
+// Non Primitive Data type --> Array, Object
+
+//string
+let name1="Chandra"
+console.log(name1);
+console.log(typeof(name1));
+
+
+//Number
+let num=98.904
+console.log(num);
+console.log(typeof(num));
+
+//boolea
+let pass=true
+console.log(pass);
+
+// undefined
+let name2;
+console.log(name2);
+console.log(typeof(num));
+
+//null
+let name3=null
+console.log(name3);
+console.log(typeof(name3));
+
+//symbol
+let id=Symbol('cp')
+let id2=Symbol('cp')
+console.log(id);
+console.log(id2);
+console.log(id==id2);
+
+//bigint
+let bigint=1234567890456788457876509876543
+console.log(bigint);
+
+
+
+
+
+
+
