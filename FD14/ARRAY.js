6dc33d7c4ac7a7afe4daa57bd0b89ce70419b2bc
Chandra Prakash <prakashchandra7052@gmail.com>
2026-06-23 21:11:31 +0530

    FD14 HW done

---
+++ FD14/ARRAY.js
@@ -0,0 +1,71 @@
+// Create an array 
+//  let std=[   "Chandra roy",
+//              "Abhi", 
+//              "Ayush",
+//              "Arun",
+//              "Nayak"
+//          ]
+// accesing an array
+//  console.log(std);
+          
+//  console.log(std[0]);
+//  console.log(std[1]);
+//  console.log(std[2]);
+//  console.log(std[4]);
+//  console.log(std[3]);
+
+ // length of an array
+
+//  len=std.length
+
+//  console.log("Length of Array is:", len);
+
+
+// Common methods in Array
+// let arr=[4,5,8,9,7,6,3,2,]
+
+//push a eLEMENt from last index
+//  arr.push(1)
+ // insert elemnet from first index
+//  arr.unshift(10)
+// console.log(arr);
+
+//delet element from last index
+// arr.pop()
+// console.log(arr);
+
+
+//delet element from first index
+// arr.shift()
+// console.log(arr);
+
+//includes() its give the answer true/false
+// arr.includes(45)
+// console.log(arr.includes(45));
+
+//indexOf() find the index of elements
+// find=arr.indexOf(5)
+// console.log(find);
+
+
+// Reverse the Array
+
+// let players=["KL Rahul", "Bumrah", "Virat Kohli", "Rohit Sharma", "Yuvraj Singh", "Hardik Pandya"]
+// for(let i=5; i>=0; i--){
+//     console.log(players[i]);
+    
+// }
+
+// traverse 
+// let players=["KL Rahul", "Bumrah", "Virat Kohli", "Rohit Sharma", "Yuvraj Singh", "Hardik Pandya"]
+// for(let i=0; i<=5; i++){
+//     console.log(players[i]);
+    
+// }
+
+
+
+
+
+
+
---
+++ FD14/hw.js
@@ -0,0 +1,62 @@
+// Q1. Array Create karo
+// Ek array banao jisme 5 fruits ke naam ho aur console me print karo.
+    // let fruits=[
+    //             "Mango",
+    //             "Apple",
+    //             "Guava",
+    //             "Grapes",
+    //             "Litchi"
+
+    // ]
+    // console.log(fruits);
+    
+
+
+// 👉 Q2. First aur Last Element print karo
+// Upar wale array ka pehla aur aakhri element alag-alag print karo.
+    // console.log(fruits[0]);
+    // console.log(fruits[4]);
+    
+        
+
+
+// 👉 Q3. Array me naya element add karo
+// fruits array me "Pineapple" add karo aur updated array print karo.
+        //  fruits.push("Pineapple")
+        // console.log(fruits);
+
+// 👉 Q4. Array se ek element remove karo
+// Array ka last element remove karo aur updated array print karo.
+    // fruits.pop()
+    // console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+// 👉 Q5. Numbers ka Sum nikalo
+// Ek array banao:
+// In sab numbers ka total sum nikal kar print karo.
+        let num=[01,18,45,96,10]
+        // add=(num[0]+num[1]+num[2]+num[3]+num[4])
+        // console.log(add);
+        
+
+
+// 👉 Q6. Loop ke saath Array print karo
+// for loop ka use karke numbers array ke saare elements print karo
+for(i=0;i<=4;i++){
+    console.log(num[i]);
+    
+}
